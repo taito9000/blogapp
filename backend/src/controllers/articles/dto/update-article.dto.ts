65037f144dc2edc8bf4ba6b7e77278c75a4d0bf7
taito9000 <ada.0205.tai120@gmail.com>
2025-02-27 21:08:24 +0900

    reset

--- backend/src/controllers/articles/dto/update-article.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateArticleDto } from 'src/controllers/articles/dto/create-article.dto';
-
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
--- backend/src/lib/decorator/permission.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const permission_key = 'permission';
-export const Permission = (decoratorValue: string) =>
-  SetMetadata(permission_key, decoratorValue);
